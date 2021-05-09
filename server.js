886bb1d49e70457e14127e5262b9106c2cc6bb98
JosephOlabode <josepholabode17@gmail.com>
2021-05-09 23:16:11 +0000

    server started successfully

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import bodyParser from "body-parser";
+import http from 'http';
 
 
 const app = express();
@@ -11,4 +12,23 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "*");
     next();
+});
+
+// this creates an express server.
+const server = http.createServer(app);
+
+// defining a constant port number
+const PORT = process.env.PORT || 3000;
+
+// this allows parsing of json data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/',  (req, res, next) =>{
+    res.send("Max movie server is working");
+});
+
+
+server.listen(PORT, () => {
+    console.log("Server Listening on Port: "+ PORT);
 });
